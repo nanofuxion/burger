60c2bd022fd02dc4471a8cb86ed6ba6c4cd62bda
nanofuxion <nanofuxion@users.noreply.github.com>
2019-08-14 20:19:06 -0400

    fixed JAWSDB connection

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,19 @@ require("dotenv").config();
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: process.env.MYSQL_PASSWORD,
   database: "burgers_db"
 });
+}
 
 // Make connection.
 connection.connect(function(err) {
